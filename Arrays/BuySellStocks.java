0b3ca71fec8ebb9d0648f2b468755a71658e3498
Tejesh406 <tejeswarreddy2000@gmail.com>
2026-01-22 21:40:38 +0530

    buy sell stocks and valid palindrome code

---
+++ Arrays/BuySellStocks.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class BuySellStocks {
+    public static void main(String[] args) {
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        System.out.println("Maximum Profit: " + maxProfit(prices));
+    }
+
+    private static int maxProfit(int[] prices) {
+        int profit = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min)
+                min = prices[i];
+            else
+                profit = Math.max(profit, prices[i] - min);
+        }
+
+        return profit;
+    }
+}
---
+++ Strings/ValidPalindrome.java
@@ -0,0 +1,24 @@
+package Strings;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+
+    // Time Complexity: O(n) time complexity and O(1) space complexity
+    // Two pointer approach
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
